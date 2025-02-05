ba4f4930de39b95a2c7e8b07c93f268908a6b44d
Danil <fofogogo12345689@mail.ru>
2025-02-05 13:48:02 +0500

    inin env and gitignore

--- src/config.py
+++ src/config.py
@@ -1,8 +1,14 @@
-# src/config.py
-API_TOKEN = '7905777515:AAEteXO0eCzYSjwFhDRymP1ZfZ2stn59Lw8'
-PAYMENT_PROVIDER_TOKEN = '381764678:TEST:110185'
-ADMIN_ID = 344101641
-FEEDBACK_CHANNEL_ID = -1002344913874
+from dotenv import load_dotenv
+import os
+
+# Загрузка переменных окружения из файла .env
+load_dotenv()
+
+# Получение значений переменных
+API_TOKEN = os.getenv("API_TOKEN")
+PAYMENT_PROVIDER_TOKEN = os.getenv("PAYMENT_PROVIDER_TOKEN")
+ADMIN_ID = int(os.getenv("ADMIN_ID"))
+FEEDBACK_CHANNEL_ID = int(os.getenv("FEEDBACK_CHANNEL_ID"))
 
 # Структура данных для дисциплин, вариантов и номеров практических работ
 import json
